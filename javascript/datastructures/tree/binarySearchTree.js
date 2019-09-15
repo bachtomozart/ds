cc06a3a3fc4d5bdd1bffb3b7d62889d17eae77a3
Govindarajan Panneerselvam <imgovind@live.com>
2019-09-15 02:22:53 -0700

    Added search for BST

--- javascript/datastructures/tree/binarySearchTree.js
+++ javascript/datastructures/tree/binarySearchTree.js
@@ -62,6 +62,27 @@ class BinarySearchTree {
       this.inorderTraversal(node.right, cb);
     }
   }
+
+  searchTree(data) {
+    let startNode = this.root,
+    searchResult = this.search(startNode, data);
+    searchResult !== null ? console.log(`The given value ${data} has been found`) : console.log(`The value ${data} could not be found`);
+  }
+
+  search(node, data) {
+    console.log(`search - ${node !== null ? node.data : null}`);
+    if(!node) {
+      return null;
+    } else {
+      if(data === node.data) {
+        return node;
+      } else if(data < node.data) {
+        return this.search(node.left, data);
+      } else if (data > node.data) {
+        return this.search(node.right, data);
+      }
+    }
+  }
 }
 
 const demoBinarySearchTree = () => {
@@ -79,6 +100,7 @@ const demoBinarySearchTree = () => {
   bst.insert(15);
   console.log(JSON.stringify(bst.root));
   bst.printTree();
+  bst.searchTree(9);
 }
 
 demoBinarySearchTree();
