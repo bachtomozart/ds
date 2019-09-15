e6a9cf52ac0f92c4c5757f50f45e5d4b7647f0d4
Govindarajan Panneerselvam <imgovind@live.com>
2019-09-15 13:44:23 -0700

    Finished Binary Search Tree

--- javascript/datastructures/tree/binarySearchTree.js
+++ javascript/datastructures/tree/binarySearchTree.js
@@ -47,6 +47,34 @@ class BinarySearchTree {
     }
   }
 
+  findParent(node, data) {
+    if(!node) {
+      return null;
+    } else {
+      if(data <= node.data) {
+        if(node.left) {
+          if(node.left.data === data) {
+            return node;
+          } else {
+            return this.findParent(node.left, data);
+          }
+        } else {
+          return null;
+        }
+      } else {
+        if (node.right) {
+          if (node.right.data === data) {
+            return node;
+          } else {
+            return this.findParent(node.right, data);
+          }
+        } else {
+          return null;
+        }
+      } 
+    }
+  }
+
   printTree() {
     let startNode = this.root, values = [];
     this.inorderTraversal(this.root, (node) => values.push(node.data));
@@ -85,26 +113,55 @@ class BinarySearchTree {
   }
 
   deleteNode(data) {
-    let startNode = this.root;
-    let searchNode = this.search(startNode, data);
+    let rootNode = this.root;
+    let searchNode = this.search(rootNode, data);
     if(!searchNode) {
-      console.log(`The given value ${data} could not be found`);
+      console.log(`DELETE - The given value ${data} could not be found`);
     } else {
-      let parent = this.findParent(searchNode);
+      let parentNode = this.findParent(rootNode, data);
       if(!searchNode.left && !searchNode.right) {
+        console.log(`DELETE - The given node is a leaf node, deleting it`);
+        if(parentNode.left.data === data) {
+          parentNode.left = null;
+        } else {
+          parentNode.right = null;
+        }
         searchNode = null;
       } else if (searchNode.left && searchNode.right) {
-        // find successor or predecessor
-        // recursively delete the successor/predecessor
+        console.log(`DELETE - The given node has two children ${searchNode.left.data} and ${searchNode.right.data}`);
+        // find successor
+        let successor = this.findSuccessor(searchNode, searchNode.data);
+        // recursively delete the successor
+        this.deleteNode(successor.data);
+        searchNode.data = successor.data;
+        searchNode = null;
       } else if (searchNode.left && !searchNode.right) {
-        if(parent.left.data === searchNode.data) {
-          parent.left = searchNode.left;
+        if(parentNode.left.data === searchNode.data) {
+          console.log(`DELETE - The given node has one child ${searchNode.left.data}, swapping it`);
+          parentNode.left = searchNode.left;
           searchNode = null;
         }
       } else if (!searchNode.left && searchNode.right) {
-        if(parent.right.data === searchNode.data) {
-          parent.right = searchNode.right;
+        if(parentNode.right.data === searchNode.data) {
+          console.log(`DELETE - The given node has one child ${searchNode.right.data}, swapping it`);
+          parentNode.right = searchNode.right;
           searchNode = null;
+        }
+      }
+    }
+  }
+
+  findSuccessor(node, data) {
+    if(!node) { 
+      return null;
+    } else {
+      if (node.data === data) {
+        return this.findSuccessor(node.right, data);  
+      } else { 
+        if (node.left) {
+          return this.findSuccessor(node.left, data);
+        } else {
+          return node;
         }
       }
     }
@@ -124,9 +181,22 @@ const demoBinarySearchTree = () => {
   bst.insert(2);
   bst.insert(1);
   bst.insert(15);
-  console.log(JSON.stringify(bst.root));
+  bst.insert(13);
+  bst.insert(17);
+  bst.insert(12);
+  bst.insert(14);
+  bst.insert(11);
+  bst.insert(18);
+  bst.insert(19);
+  bst.insert(16);
   bst.printTree();
   bst.searchTree(9);
+  bst.deleteNode(1);bst.printTree();console.log(JSON.stringify(bst.root));
+  bst.deleteNode(7);bst.printTree();console.log(JSON.stringify(bst.root));
+  bst.deleteNode(13);bst.printTree();console.log(JSON.stringify(bst.root));
+  bst.deleteNode(18);bst.printTree();console.log(JSON.stringify(bst.root));
+  bst.insert(13);bst.printTree();console.log(JSON.stringify(bst.root));
+
 }
 
 demoBinarySearchTree();
