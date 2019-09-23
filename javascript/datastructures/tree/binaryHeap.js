a0a806da376f88549abe565b707c8e776d0610a3
Govindarajan Panneerselvam <imgovind@live.com>
2019-09-23 02:34:09 -0700

    Fixed MinHeap and added MaxHeap

--- javascript/datastructures/tree/binaryHeap.js
+++ javascript/datastructures/tree/binaryHeap.js
@@ -14,9 +14,12 @@ class MinHeap {
   }
 
   insert(data) {
+    console.log(`Inserting -> last: ${this.last} - data: ${data}`)
     this.array[this.last] = data;
+    console.log(`INSERTED BF -> last - ${this.last} - array: ${JSON.stringify(this.array)}`);
     this.balanceHeapBottomUp(this.last, this.array[this.last]);
     this.last++;
+    console.log(`INSERTED AF -> last - ${this.last} - array: ${JSON.stringify(this.array)}`);
   }
 
   balanceHeapBottomUp(child, childData) {
@@ -26,7 +29,7 @@ class MinHeap {
       if(parentData > childData) {
         this.array[child] = parentData;
         this.array[parent] = childData;
-        this.balanceHeap(parent, this.array[parent]);
+        this.balanceHeapBottomUp(parent, this.array[parent]);
       }
     }
   }
@@ -34,7 +37,7 @@ class MinHeap {
   balanceHeapTopDown(parent, parentData) {
     if(parent < this.size) {
       let leftChild = this.left(parent),
-        leftChildData = this.array[leftChild];
+        leftChildData = this.array[leftChild],
         rightChild = this.right(parent),
         rightChildData = this.array[rightChild];
       if(!leftChildData && rightChildData) {
@@ -67,17 +70,20 @@ class MinHeap {
   }
 
   extract() {
-    console.log(`Extracted -> ${this.array[1]}`);
-    this.array[1] = this.array[this.last];
+    console.log(`Extracting top value -> last: ${this.last} - value: ${this.array[1]} - array: ${JSON.stringify(this.array)}`);
+    this.array[1] = this.array[this.last-1];
+    this.array[this.last-1] = null;
     this.last--;
+    console.log(`EXTRACTED BF -> last: ${this.last} - array: ${JSON.stringify(this.array)}`);
     this.balanceHeapTopDown(1, this.array[1]);
+    console.log(`EXTRACTED AF -> last: ${this.last} - array: ${JSON.stringify(this.array)}`);
   }
 
   peak() {
     console.log(`Peaking -> ${this.array[1]}`);
   }
 
-  getParent(pos) {
+  parent(pos) {
     if(pos % 2 === 1) {
       pos--;
     }
@@ -96,3 +102,139 @@ class MinHeap {
     return this.size;
   }
 }
+
+class MaxHeap {
+  constructor(length) {
+    this.last = 1;
+    this.size = length; 
+    this.array = new Array(length);
+  }
+
+  destroy() {
+    this.last = null;
+    this.array = null;
+    this.size = 0;
+  }
+
+  insert(data) {
+    console.log(`Inserting -> last: ${this.last} - data: ${data}`)
+    this.array[this.last] = data;
+    console.log(`INSERTED BF -> last - ${this.last} - array: ${JSON.stringify(this.array)}`);
+    this.balanceHeapBottomUp(this.last, this.array[this.last]);
+    this.last++;
+    console.log(`INSERTED AF -> last - ${this.last} - array: ${JSON.stringify(this.array)}`);
+  }
+
+  balanceHeapBottomUp(child, childData) {
+    if(child > 0) {
+      let parent = this.parent(child),
+        parentData = this.array[parent];
+      if(parentData < childData) {
+        this.array[child] = parentData;
+        this.array[parent] = childData;
+        this.balanceHeapBottomUp(parent, this.array[parent]);
+      }
+    }
+  }
+
+  balanceHeapTopDown(parent, parentData) {
+    if(parent < this.size) {
+      let leftChild = this.left(parent),
+        leftChildData = this.array[leftChild],
+        rightChild = this.right(parent),
+        rightChildData = this.array[rightChild];
+      if(!leftChildData && rightChildData) {
+        if(parentData < rightChildData) {
+          this.array[rightChild] = parentData;
+          this.array[parent] = rightChildData;
+          this.balanceHeapTopDown(rightChild, this.array[rightChild]);
+        }
+      } else if (!rightChildData && leftChildData) {
+        if(parentData < leftChildData) {
+          this.array[leftChild] = parentData;
+          this.array[parent] = leftChildData;
+          this.balanceHeapTopDown(leftChild, this.array[leftChild]);
+        }
+      } else if(leftChildData && 
+        rightChildData &&
+        (parentData < leftChildData || 
+        parentData < rightChildData)) {
+        if(leftChildData > rightChildData) {
+          this.array[leftChild] = parentData;
+          this.array[parent] = leftChildData;
+          this.balanceHeapTopDown(leftChild, this.array[leftChild]);
+        } else  {
+          this.array[rightChild] = parentData;
+          this.array[parent] = rightChildData;
+          this.balanceHeapTopDown(rightChild, this.array[rightChild]);
+        }
+      }
+    }
+  }
+
+  extract() {
+    console.log(`Extracting top value -> last: ${this.last} - value: ${this.array[1]} - array: ${JSON.stringify(this.array)}`);
+    this.array[1] = this.array[this.last-1];
+    this.array[this.last-1] = null;
+    this.last--;
+    console.log(`EXTRACTED BF -> last: ${this.last} - array: ${JSON.stringify(this.array)}`);
+    this.balanceHeapTopDown(1, this.array[1]);
+    console.log(`EXTRACTED AF -> last: ${this.last} - array: ${JSON.stringify(this.array)}`);
+  }
+
+  peak() {
+    console.log(`Peaking -> ${this.array[1]}`);
+  }
+
+  parent(pos) {
+    if(pos % 2 === 1) {
+      pos--;
+    }
+    return pos / 2;
+  }
+
+  left(pos) {
+    return 2 * pos;
+  }
+
+  right(pos) {
+    return 2 * pos + 1;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+const demoMinHeap = () => {
+  let heap = new MinHeap(10);
+  heap.insert(30);
+  heap.insert(20);
+  heap.insert(40);
+  heap.insert(50);
+  heap.insert(60);
+  heap.insert(10);
+  heap.peak();
+  heap.extract();
+  heap.extract();
+  heap.extract();
+  heap.extract();
+  heap.extract();
+}
+const demoMaxHeap = () => {
+  let heap = new MaxHeap(10);
+  heap.insert(30);
+  heap.insert(20);
+  heap.insert(40);
+  heap.insert(50);
+  heap.insert(60);
+  heap.insert(10);
+  heap.peak();
+  heap.extract();
+  heap.extract();
+  heap.extract();
+  heap.extract();
+  heap.extract();
+}
+// demoMinHeap();
+demoMaxHeap();
