495b2ca877d044214f9636c39720181df84aa460
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-08 11:02:14 -0700

    Removed unnecessary conditionals in fibonacci

--- javascript/algorithms/divideAndConquer/fibonacci.js
+++ javascript/algorithms/divideAndConquer/fibonacci.js
@@ -2,14 +2,14 @@
 
 class Fibonacci {
   constructor() {
-    this.map = new Map();
+    this.map = new Map([[1, 0], [2, 1]]);
   }
 
   fibonacci(number) {
-    if (number < 1) return null;
-    if (number === 1) return 0;
-    if (number === 2) return 1;
-    if (this.map.has(number)) return this.map.get(number);
+    if (number < 1) 
+      return null;
+    if (this.map.has(number)) 
+      return this.map.get(number);
     let result = this.fibonacci(number - 2) + this.fibonacci(number - 1);
     this.map.set(number, result);
     return result;
