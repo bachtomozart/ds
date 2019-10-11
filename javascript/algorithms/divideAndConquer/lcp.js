48734461d478edbb840a4ff022cdd6a0e58eb7c1
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-11 16:50:36 -0700

    Finished LCP

--- javascript/algorithms/divideAndConquer/lcp.js
+++ javascript/algorithms/divideAndConquer/lcp.js
@@ -11,59 +11,23 @@ class LCP {
     console.log(`The Longest common palindrome subsequence of ${string} is ${result}`);
   }
 
-  findLCP(string) {
-    if(!string[0]) return new Array();
-    let result1 = new Array(string[0]), result2 = new Array();
-    let tempResult = this.findLCP(string.slice(1));
-    result1.push(...tempResult);
-    result2.push(...tempResult);
-    let string1 = result1.join('');
-    let reverseString1 = result1.reverse().join('');
-    let return1 = false;
-    if(result1 && 
-      result1.length && 
-      string1 === reverseString1) {
-      if(result1.length > this.max) {
-        this.max = result1.length;
-        this.maxSubSequence = result1;
-      }
-      return1 = true;
-    } 
-    let return2 = false;
-    let string2 = result2.join('');
-    let reverseString2 = result2.reverse().join('');
-    if (result2 && 
-      result2.length && 
-      string2 === reverseString2) {
-      if(result2.length > this.max) {
-        this.max = result2.length;
-        this.maxSubSequence = result2;
-      }
-      return2 = true;
+  findLCP(string, start = 0, end = (string.length - 1)) {
+    if(start > end) return 0;
+    if(start === end) return 1;
+    if(string[start] === string[end]) {
+      return 2 + this.findLCP(string, start + 1, end - 1);
     }
-    if(return1 && !return2) {
-      return result1;
-    } else if (!return1 && return2) {
-      return result2
-    } else if (return1 && return2) {
-      if(result1.length > result2.length) {
-        return result1;
-      } else {
-        return result2;
-      }
-    }
+    let different1 = this.findLCP(string, start, end - 1);
+    let different2 = this.findLCP(string, start + 1, end);
+    return Math.max(different1, different2);
   }
-
-  getString(array) {
-    return array.reduce((acc,item) => acc + item, []);
-  }
-
 }
 
 const demo = () => {
   let lcp = new LCP();
   lcp.getLCP('MONIO');
   lcp.getLCP('MONACO');
+  lcp.getLCP('MADAM');
 }
 
 demo();
