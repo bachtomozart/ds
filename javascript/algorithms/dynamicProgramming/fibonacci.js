e4fc26421b310cb55eedd4b500b3845abbde6254
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-13 18:44:10 -0700

    Added all three fibonacci implementations

--- javascript/algorithms/dynamicProgramming/fibonacci.js
+++ javascript/algorithms/dynamicProgramming/fibonacci.js
@@ -1,9 +1,11 @@
 'use strict'
 
 class Fibonacci {
+
   constructor() {
     this.array = [0, 1]
     this.computed = new Set([0, 1]);
+    this.map = new Map([[1, 0], [2, 1]]);
   }
 
   getFibonacci(n) {
@@ -11,6 +13,23 @@ class Fibonacci {
     console.log(`The fibonacci for ${n} is ${result}`);
   }
 
+  // recursive
+  fib(number) {
+    return this.fib(number-1) + this.fib(number-2);
+  }
+
+  // Top Down (Memoization)
+  fibonacci(number) {
+    if (number < 1) 
+      return null;
+    if (this.map.has(number)) 
+      return this.map.get(number);
+    let result = this.fibonacci(number - 1) + this.fibonacci(number - 2);
+    this.map.set(number, result);
+    return result;
+  }
+
+  // Bottom up (Tabulation)
   findFibonacci(n) {
     if(this.computed.has(n)) return this.array[n];
     for(let i = this.array.length; i < n; i++) {
