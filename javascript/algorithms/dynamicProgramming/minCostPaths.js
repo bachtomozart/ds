b077f45e8a143f6e5929277fa2060d99c76bb579
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-19 19:54:45 -0700

    Finished bottom up approach for Minimum Cost Paths in an array Finished Tutorial, Astala vista baby!

--- javascript/algorithms/dynamicProgramming/minCostPaths.js
+++ javascript/algorithms/dynamicProgramming/minCostPaths.js
@@ -2,6 +2,8 @@
 
 const Dynamic = require('./dynamic');
 
+
+
 class MCP extends Dynamic {
 
   constructor() {
@@ -18,35 +20,50 @@ class MCP extends Dynamic {
     let recursive = this.findMCPRecursive(input, maxCost);
     this.initializeTDArray(input);
     let topDown = this.findMCPTopDown(input, maxCost);
-    let result = recursive;
-    console.log(`The MCP is ${result}`);
+    let bottomUp = this.findMCPBottomUp(input, maxCost);
+    let result = bottomUp;
+    console.log(`The MCP for given cost ${maxCost} is ${result}`);
     this.printCounts();
   }
 
-  findMCPRecursive(input, maxCost, cost=0, paths=0, i=(input.length-1), j=(input.length-1)) {
+  findMCPRecursive(input, cost, i=(input.length-1), j=(input.length-1)) {
     this.recursiveCount++;
-    if(i < 0 || j < 0) return 0;
-    let left = this.findMCPRecursive(input, maxCost, cost + input[i][j], paths, i, j-1);
-    let up = this.findMCPRecursive(input, maxCost, cost + input[i][j], paths, i-1, j);
-    paths = up + left;
-    if(i===0 && j===0 && (cost + input[0][0]) === maxCost) {
-      paths = paths + 1;
-    }
-    return paths;
+    if(cost < 0) 
+      return 0;
+    if(i === 0 && j === 0) 
+      return ((input[0][0] - cost) === 0) ? 1 : 0;
+    if(i === 0) 
+      return this.findMCPRecursive(input, cost - input[i][j], i, j-1);
+    if(j === 0) 
+      return this.findMCPRecursive(input, cost - input[i][j], i-1, j);
+    let left = this.findMCPRecursive(input, cost - input[i][j], i, j-1);
+    let up = this.findMCPRecursive(input, cost - input[i][j], i-1, j);
+    return left + up;
   }
 
-  findMCPTopDown(input, maxCost, cost=0, paths=0, i=(input.length-1), j=(input.length-1)) {
+  findMCPTopDown(input, cost, i=(input.length-1), j=(input.length-1)) {
     this.topDownCount++;
-    if(i < 0 || j < 0) return 0;
-    if(this.tdArray[i][j]) return this.tdArray[i][j];
-    let left = this.findMCPTopDown(input, maxCost, cost + input[i][j], paths, i, j-1);
-    let up = this.findMCPTopDown(input, maxCost, cost + input[i][j], paths, i-1, j);
-    paths = up + left;
-    if(i===0 && j===0 && (cost + input[0][0]) === maxCost) {
-      paths = paths + 1;
-    }
-    this.tdArray[i][j] = paths;
-    return paths;
+    if(cost < 0) 
+      return 0;
+    if(i === 0 && j === 0) 
+      return ((input[0][0] - cost) === 0) ? 1 : 0;
+    // if(this.tdArray[i][j]) 
+      // return this.tdArray[i][j];
+    if(this.map.has(this.getKey(i,j,cost))) return this.map.get(this.getKey(i,j,cost));
+    if(i === 0) 
+      return this.findMCPTopDown(input, cost - input[i][j], 0, j-1);
+    if(j === 0) 
+      return this.findMCPTopDown(input, cost - input[i][j], i-1, 0);
+    let left = this.findMCPTopDown(input, cost - input[i][j], i, j-1);
+    let up = this.findMCPTopDown(input, cost - input[i][j], i-1, j);
+    let result = left + up;
+    // this.tdArray[i][j] = result;
+    this.map.set(this.getKey(i,j,cost),result);
+    return result;
+  }
+
+  getKey(i,j,cost) {
+    return i + '-' + j + '-' + cost;
   }
 
   initializeTDArray(input) {
@@ -57,6 +74,91 @@ class MCP extends Dynamic {
         this.tdArray[i][j] = null;
       }
     }
+  }
+
+  findMCPBottomUp(input, maxCost) {
+    this.initializeArray(input, maxCost);
+    for(let i=(input.length-1);i>=0;i--) {
+      for(let j=(input.length-1);j>=0;j--) {
+        this.bottomUpCount++;
+        let cell = this.array[i][j];
+        cell.calculateForRightCell(this.array[i][j+1]);
+        cell.calculateForDownCell(this.array[i+1][j]);
+        console.log(JSON.stringify(this.array[i][j]));
+      }
+    }
+    return this.array[0][0].calculatePathways();
+  }
+
+  initializeArray(input, maxCost) {
+    this.array = new Object();
+    for(let i=0;i<=input.length;i++) {
+      this.array[i] = new Object();
+      for(let j=0;j<=input.length;j++) {
+        if(i === input.length-1 && j === input.length -1) {
+          this.array[i][j] = new MCPNode(input[i][j], Array(1).fill(maxCost), new Array());
+        } else {
+          this.array[i][j] = new MCPNode((input[i] && input[i][j] ? input[i][j] : 0));
+        }
+      }
+    }
+    this.printArray(input);
+  }
+
+  printArray(input) {
+    console.log('-----------------------');
+    for(let i=0;i<=input.length;i++) {
+      let string = '';
+      for(let j=0;j<=input.length;j++) {
+        let value = this.array[i] 
+        && this.array[i][j] 
+        && this.array[i][j].cost !== null 
+        ? this.array[i][j].cost 
+        : 'N';
+        string += '[' + value + ']\t';
+      }
+      console.log(string);
+    }
+    console.log('-----------------------');
+  }
+
+}
+
+class MCPNode {
+
+  constructor(cost, down = new Array(), right = new Array()) {
+    this.cost = cost;
+    this.down = down;
+    this.right = right;
+  }
+
+  calculateForDownCell(cell) {
+    for(let downCost of cell.down) {
+      this.down.push(downCost-cell.cost);
+    }
+    for(let rightCost of cell.right) {
+      this.down.push(rightCost-cell.cost);
+    }
+  }
+
+  calculateForRightCell(cell) {
+    for(let downCost of cell.down) {
+      this.right.push(downCost-cell.cost);
+    }
+    for(let rightCost of cell.right) {
+      this.right.push(rightCost-cell.cost);
+    }
+  }
+
+  calculatePathways() {
+    let downPaths = 0, rightPaths = 0;
+    for(let downCost of this.down) {
+      if(downCost === this.cost) downPaths++;
+    }
+    for(let rightCost of this.right) {
+      if(rightCost === this.cost) rightPaths++;
+    }
+    return downPaths + rightPaths;
   }
 
 }
