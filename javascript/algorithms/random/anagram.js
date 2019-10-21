e2830b601fdf39f4ebc4b68051adad3e7f401753
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-20 19:12:12 -0700

    Added my original solution to anagram

--- javascript/algorithms/random/anagram.js
+++ javascript/algorithms/random/anagram.js
@@ -8,31 +8,33 @@ class Anagram {
   isAnagram(string1, string2) {
     let result1 = this.validAnagram1(string1, string2);
     let result2 = this.validAnagram2(string1, string2);
+    let result3 = this.validAnagram3(string1, string2);
     console.log(`The given inputs ${string1} and ${string2} is ${result1 ? "an anagram" : "not an anagram"}`);
     console.log(`The given inputs ${string1} and ${string2} is ${result2 ? "an anagram" : "not an anagram"}`);
+    console.log(`The given inputs ${string1} and ${string2} is ${result3 ? "an anagram" : "not an anagram"}`);
   }
 
   validAnagram1(string1, string2) {
+    if(string1.length !== string2.length)
+      return false;
     let map1 = new Map();
     let map2 = new Map();
-
     for(let char of string1) {
       map1.has(char) ? map1.set(char, map1.get(char) + 1) : map1.set(char, 1);
     }
-
     for(let char of string2) {
       map2.has(char) ? map2.set(char, map2.get(char) + 1) : map2.set(char, 1);
     }
-
     for(let [key,value] of map1.entries()) {
       if(!map2.has(key)) return false;
       if(map2.get(key) !== value) return false;
     }
-
     return true;
   }
 
   validAnagram2(string1, string2){
+    if(string1.length !== string2.length)
+      return false;
     let map1 = {};
     let map2 = {};
     for(let char of string1) {
@@ -47,6 +49,20 @@ class Anagram {
         if(map2[key] !== map1[key]) return false;
     }
     return true;
+  }
+
+  validAnagram3(string1, string2) {
+    if(string1.length !== string2.length)
+      return false;
+    let map = new Map();
+    for(let char of string1) {
+      map.has(char) ? map.set(char, map.get(char) + 1) : map.set(char, 1);
+    }
+    for(let char of string2) {
+      if(!map.has(char)) return false;
+      map.get(char) > 1 ? map.set(char, map.get(char) - 1) : map.delete(char);
+    }
+    return map.size === 0;
   }
 }
 
