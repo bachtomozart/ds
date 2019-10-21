41d6ae53cd5be245fe5f52aa7248a150bef87d6b
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-20 22:22:38 -0700

    Added Max Sub Array problem

--- javascript/algorithms/random/countUniques.js
+++ javascript/algorithms/random/countUniques.js
@@ -9,8 +9,10 @@ class CountUnique {
   getUniques(input) {
     let result1 = this.findUniques1(input);
     let result2 = this.findUniques2(input);
+    let result3 = this.findUniques3(input);
     console.log(`The number of unique values in ${JSON.stringify(input)} is ${result1}`);
     console.log(`The number of unique values in ${JSON.stringify(input)} is ${result2}`);
+    console.log(`The number of unique values in ${JSON.stringify(input)} is ${result3}`);
   }
 
   findUniques1(input) {
@@ -37,12 +39,27 @@ class CountUnique {
     return result;
   }
 
+  findUniques3(input){
+    if(input.length <= 1) return input.length;
+    let i=0, j=1;
+    while(j < input.length) {
+        if(input[i] !== input[j]) {
+            i++;
+            input[i] = input[j];
+        }
+        j++;
+    }
+    return i + 1;
+  }
+
 }
 
 const demo = () => {
   let cu = new CountUnique();
   cu.getUniques(new Array(1,1,1,1,1,1,1,2));
   cu.getUniques(new Array(1,2,3,4,4,4,7,7,12,12,13));
+  cu.getUniques([11]);
+  cu.getUniques([]);
 }
 
 demo();
---
+++ javascript/algorithms/random/maxSubArray.js
@@ -0,0 +1,39 @@
+'use strict'
+
+class MaxSubArray {
+
+  constructor() {
+
+  }
+
+  getMSA(input, num) {
+    let result = this.findMSA(input, num);
+    console.log(`The Max sum for ${num} in ${JSON.stringify(input)} is ${result}`);
+  }
+
+  findMSA(input, num) {
+    if(num > input.length) return null;
+    let globalMax = 0, currentMax = 0;
+    for(let i=0;i<num;i++) {
+      currentMax += input[i];
+    }
+    globalMax = currentMax;
+    for(let i=num;i<input.length;i++) {
+      currentMax = currentMax - input[i-num] + input[i];
+      globalMax = Math.max(currentMax, globalMax);
+    }
+    return globalMax;
+  }
+
+}
+
+const demo = () => {
+  let msa = new MaxSubArray();
+  msa.getMSA(new Array(1,2,5,2,8,1,5), 2); // 10
+  msa.getMSA(new Array(1,2,5,2,8,1,5), 4); // 17
+  msa.getMSA(new Array(4,2,1,6),1); // 6
+  msa.getMSA(new Array(4,2,1,6,2),4); // 13
+  msa.getMSA(new Array(),4); // null
+}
+
+demo();
