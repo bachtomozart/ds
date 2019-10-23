7497b84e640e35e07d64db7d2bd21ce8d629aa1b
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-22 23:58:23 -0700

    Added Same Frequency Problem

---
+++ javascript/helloWorld/scratchpad.js
@@ -0,0 +1,61 @@
+//let array = new Object();
+//array[0] = new Object();
+//array[0][0] = '00';
+//array[0][1] = '01';
+//array[1] = new Object();
+//array[1][0] = '10';
+//array[1][1] = '11';
+//console.log(JSON.stringify(array));
+//let test = array[0][2] || 0;
+//console.log(JSON.stringify(test));
+//
+//let val = 'Hello';
+//console.log(val.length);
+//
+//class ListNode {
+//  constructor(val, next = null) {
+//    this.val = val;
+//    this.next = next;
+//  }
+//}
+//let nums = '801'.split('');
+//let result = new ListNode(nums.splice(0,1));
+//console.log(JSON.stringify(result));
+//console.log(JSON.stringify(nums));
+
+// let nums = [3,1,5,8];
+
+// let map = new Map([
+  // [-1,1],
+  // [nums.length+1, 1]
+// ])
+
+// console.log(JSON.stringify([...map]));
+
+// let map = new Map();
+// map.set('a', 'A value');
+// map.set('b', 'B value');
+// console.log(map.size)
+
+// let m = 4294967295;
+// let n = 4;
+// let k = Math.ceil(Math.log2(m/n));
+// console.log(`k -> ${k}`);
+
+// let arr = new Array();
+// let ar = [];
+
+// console.log(JSON.stringify(arr));
+// console.log(JSON.stringify(ar));
+
+// let result = ['aide'];
+// let value = ['ir','isle'];
+// let transformed = value.map((item) => 'a' + item);
+// console.log(JSON.stringify(transformed));
+// result.push(...transformed);
+// console.log(JSON.stringify(result));
+
+//
+// let myArray = [1,2,3,4,5];
+// myArray.shift();
+// console.log(myArray);
