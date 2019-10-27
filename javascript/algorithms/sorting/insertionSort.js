1eafe2e2a9309ed16435856ba97859d6eab7bfcf
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-27 03:20:29 -0700

    Added insertion sort

--- javascript/algorithms/sorting/insertionSort.js
+++ javascript/algorithms/sorting/insertionSort.js
@@ -5,6 +5,13 @@ const Sort = require('./sort');
 class InsertionSort extends Sort {
   sort(input) {
     console.log(`Before Sort -> ${input}`);
+    for (let i = 1; i < input.length; i++) {
+      let currentVal = input[i];
+      for (var j = i - 1; j >= 0 && input[j] > currentVal; j--) {
+        input[j+1] = input[j];
+      }
+      input[j + 1] = currentVal;
+    }
     console.log(`After Sort -> ${input}`);
     return input;
   }
