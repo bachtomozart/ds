1b78749485804e8f0fcf56b5a891039ea4eb8b5f
Govindarajan Panneerselvam <imgovind@live.com>
2019-10-27 02:52:36 -0700

    Finished selection sort

--- javascript/algorithms/sorting/selectionSort.js
+++ javascript/algorithms/sorting/selectionSort.js
@@ -5,6 +5,14 @@ const Sort = require('./sort');
 class SelectionSort extends Sort {
   sort(input) {
     console.log(`Before Sort -> ${input}`);
+    for(let i=0;i<input.length;i++) {
+      let smallest = i;
+      for(let j = i+1; j<input.length;j++) {
+        if(input[j] < input[i] && input[j] < input[smallest]) 
+          smallest = j;
+      }
+      this.swap(input, i, smallest);
+    }
     console.log(`After Sort -> ${input}`);
     return input;
   }
