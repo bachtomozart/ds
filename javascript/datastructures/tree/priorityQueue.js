a6bc840ca03f464af6e27c6a96d4dba855aed0ba
Govindarajan Panneerselvam <imgovind@live.com>
2019-11-01 02:27:35 -0700

    Added PriorityQueue

---
+++ javascript/datastructures/tree/priorityQueue.js
@@ -0,0 +1,97 @@
+'use strict'
+
+class PriorityQueue {
+
+  constructor(comparator) {
+    this.comparator = comparator;
+    this.store = [];
+  }
+
+  peek() {
+    return this.store[0];
+  }
+
+  enqueue(value) {
+    this.store.push(value);
+    this.heapifyUp();
+  }
+
+  heapifyUp(childPos = this.store.length - 1) {
+    if(childPos === 0) return;
+    let parentPos = this.getParentPos(childPos);
+    if(!this.comparator(this.store[parentPos], this.store[childPos])) {
+      this.swap(parentPos, childPos);
+      this.heapifyUp(parentPos);
+    }
+    return;
+  }
+
+  dequeue() {
+    let result = this.store[0];
+    this.store[0] = this.store.pop();
+    this.heapifyDown();
+    return result;
+  }
+
+  heapifyDown(parentPos = 0) {
+    if(parentPos >= this.store.length) return;
+    let leftPos = this.getLeftPos(parentPos);
+    let rightPos = this.getRightPos(parentPos);
+    if(!this.store[leftPos] && !this.store[rightPos]) {
+      return;
+    } else if (this.store[leftPos] && !this.store[rightPos]) {
+      if(!this.comparator(this.store[parentPos], this.store[leftPos])) {
+        this.swap(parentPos, leftPos);
+        this.heapifyDown(leftPos);
+      }
+    } else if (!this.store[leftPos] && this.store[rightPos]) {
+      if(!this.comparator(this.store[parentPos], this.store[rightPos])) {
+        this.swap(parentPos, rightPos);
+        this.heapifyDown(rightPos);
+      }
+    } else if(!this.comparator(this.store[parentPos], this.store[leftPos]) ||
+    !this.comparator(this.store[parentPos], this.store[rightPos])) {
+      if(this.store[leftPos] > this.store[rightPos]) {
+        this.swap(parentPos, leftPos);
+        this.heapifyDown(leftPos);
+      } else {
+        this.swap(parentPos, rightPos);
+        this.heapifyDown(rightPos);
+      }
+    }
+  }
+
+  getParentPos(childPos) {
+    if(childPos % 2 === 1) return (childPos - 1) / 2;
+    return (childPos - 2) / 2;
+  }
+
+  getLeftPos(parentPos) {
+    return (2 * parentPos) + 1;
+  }
+
+  getRightPos(parentPos) {
+    return (2 * parentPos) + 2;
+  }
+
+  swap(parentPos, childPos) {
+    [this.store[parentPos], this.store[childPos]] = [this.store[childPos], this.store[parentPos]];
+  }
+}
+
+class MinPriorityQueue extends PriorityQueue {
+  constructor() {
+    super((a,b) => a < b);
+  }
+}
+
+class MaxPriorityQueue extends PriorityQueue {
+  constructor() {
+    super((a,b) => a > b);
+  }
+}
+
+const demo = () => {
+  let minPriorityQueue = new MinPriorityQueue();
+  let maxPriorityQueue = new MaxPriorityQueue();
+}
