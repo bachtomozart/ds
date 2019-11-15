ff012383f5f2a5a37862efa005633f58a47da31c
Govindarajan Panneerselvam <imgovind@live.com>
2019-11-14 16:07:47 -0800

    Finished LC problem

--- javascript/algorithms/leetcode/14-longestCommonPrefix.js
+++ javascript/algorithms/leetcode/14-longestCommonPrefix.js
@@ -6,9 +6,21 @@
  * https://leetcode.com/problems/longest-common-prefix/
  */
 
-let map = new Set();
 function longestCommonPrefix(inputs) {
-  
+  let globalSet = new Set([...inputs[0]]);
+  for (let i = 1; i < inputs.length; i++) {
+    let currentSet = new Set();
+    for (let j = 0; j < inputs[i].length; j++) {
+      if (globalSet.has(inputs[i][j])) {
+        currentSet.add(inputs[i][j])
+      }
+    }
+    globalSet = new Set([...currentSet])
+  }
+  let result = [...globalSet].join('');
+  console.log(`${result}`);
+  return result;
 }
 
 longestCommonPrefix(['flower', 'flow', 'flight']);
+longestCommonPrefix(['race', 'a', 'car']);
